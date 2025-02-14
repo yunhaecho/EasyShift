81fa9253cf58760806f6b7314545ace21fc93f04
yuunhae <susu492@naver.com>
2025-02-15 02:12:09 +0900

    Montly Summary 반영안한 캘린더 완료

--- src/app/employees/components/contactInfoItems.tsx
+++ src/app/employees/components/contactInfoItems.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image';
 
-
-// import PhoneImg from '../../../assets/phone.svg';
-
 interface ContactInfoProps {
     icon: string,
     category: string,
@@ -10,17 +7,17 @@ interface ContactInfoProps {
 }
 export default function ContactInfoItems({icon,category, contactInfo} : ContactInfoProps) {
     return (
-        <div className="flex flex-row w-full h-[70px] border border-black">
-            <div className="bg-#E0E7FF w-[70px] h-full rounded-xl flex flex-col items-center justify-center mr-16">
+        <div className="flex flex-row w-full h-60 md:mb-20">
+            <div className="bg-primary-100 w-60 h-full rounded-xl flex flex-col items-center justify-center md:mr-20">
                 <Image 
                     src={icon}
-                    width={16}
-                    height={16}
+                    width={20}
+                    height={20}
                     alt='Phone Icon'/>
             </div>
-            <div className='flex flex-col justify-start items-start color-##6B7280 border border-black'>
-                <div className=' flex-[1] w-full flex items-center border border-black text-sm'>{category}</div>
-                <div className=' flex-[1] flex w-full items-center  border border-black text-base font-semibold'>{contactInfo}</div>
+            <div className='flex flex-col justify-start items-start'>
+                <span className=' flex-[1] w-full flex items-center body-14-400 text-gray-600'>{category}</span>
+                <span className=' flex-[1] flex w-full items-center body-16-500'>{contactInfo}</span>
             </div>
         </div>
 
