469b29fc0988ed5260dd9c82566c38aa77a82d02
yuunhae <susu492@naver.com>
2025-02-16 14:49:29 +0900

    Feat(calendar): 초기 모의 데이터 기반 캘린더 구현 및 스케줄 하이라이팅 - 캘린더의 날짜 배열 생성 로직 구현 (이전/현재/다음 달 포함) - 날짜 비교를 통한 스케줄 표시 기능 추가 (모의 데이터) - 추후 API 연동 및 근무유형에 따른 색상 변경 기능 추가  예정

--- src/app/employees/components/calendar.tsx
+++ src/app/employees/components/calendar.tsx
@@ -3,9 +3,8 @@ import LeftArrowIcon from '@/../public/icons/left-arrow.svg';
 import { useState } from 'react';
 
 export default function Calendar() {
-
-    const [currentMonth,setCurrentMonth ] = useState(new Date().getMonth());
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [currentMonth, setCurrentMonth ] = useState(new Date().getMonth() + 1); 
     const weekName = ['Sun', 'Mon','Tue','Wed','Thr','Fri','Sat'];
     const monthName = [
       'January',
@@ -20,32 +19,88 @@ export default function Calendar() {
       'October',
       'November',
       'December',];
-
-    // const schedule = [
-    //   { date: '2025-03-04', shift: 'open' },
-    //   { date: '2024-03-08', shift: 'closing' },
-    //   { date: '2024-03-12', shift: 'middle' },
-    // ];
     
+    //mock data
+      const schedules = [
+        {
+          "id": 101,
+          "scheduleName": "야간 근무",
+          "shifts": [
+            {
+              "id": 201,
+              "shiftDate": "2025-02-05",
+              "shiftName": "1교대",
+              "startTime": "12:00",
+              "endTime": "15:00"
+            },
+          ]
+        },
+          {
+            "id": 102,
+            "scheduleName": "주간 근무",
+            "shifts": [
+                {
+                    "id": 203,
+                    "shiftDate": "2025-02-08",
+                    "shiftName": "2교대",
+                    "startTime": "08:00",
+                    "endTime": "12:00"
+                }
+            ]
+        },
+    ];
+
     //날짜: date 요일: days 
-    const currentDate = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`
-    console.log(currentDate);
-    //현재 월 연도
-    const currentMonthLastDate = new Date(currentYear, currentMonth+1, 0).getDate();
-    const currentMonthDates = Array.from({ length: currentMonthLastDate }, (_,index) => index+1); 
-
-    const firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay(); //0(일요일) ~ 6(토요일)
+    const workInfoArr = schedules.map(i => i.shifts).flat(); //array
     
-    const prevMonthDates = Array.from({length: firstDayofMonth}, (_,i)=> currentMonthDates.length - firstDayofMonth + i +1);
+    //현재 월 연도
+    const currentMonthLastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const currentMonthDates = Array.from({ length: currentMonthLastDate }, (_,index) => index + 1); 
+    const fstOfCurrentMonth = new Date(currentYear, currentMonth, 1).getDay(); //0(일요일) ~ 6(토요일)
+    
+    const prevMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    const prevYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    const prevMonthDates = Array.from({length: fstOfCurrentMonth}, (_,i)=> currentMonthDates.length - fstOfCurrentMonth + i + 1);
+
+    const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
+    const nextYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+
+    //남은 그리드를 채우기 위한 다음 달 날짜
+    const totalCells = 42;
+    const remainCells = totalCells - (prevMonthDates.length + currentMonthDates.length);    
+    const nextMonthDates = Array.from({length: remainCells}, (_,i)=> i + 1);
+    
+    //0을 추가해서 월/일을 두 자리로 만들기
+    const addZero = (num: number) => {
+      return String(num).padStart( 2 ,'0');
+    }
+
+    // 이전 달 데이터 : "2025-01-15"
+    const prevMonthDatesArr = prevMonthDates.map( day => ({
+      date: `${prevYear}-${addZero(prevMonth + 1)}-${addZero(day)}`,
+      isCurrentMonth: false 
+    }));
+
+    //현재 달 데이터 
+    const currentMonthDatesArr = currentMonthDates.map( day => ({
+      date : `${currentYear}-${addZero(currentMonth + 1)}-${addZero(day)}`, 
+      isCurrentMonth: true
+    }));
+
+    //다음 달 데이터 
+    const nextMonthDatesArr = nextMonthDates.map(day => ({
+      date : `${nextYear}-${addZero(nextMonth + 1)}-${addZero(day)}`, 
+      isCurrentMonth: false 
+    }))
 
     const clickPrev = () => {
       if(currentMonth === 0) { //1월이면
         const prevMonth = 11; //12월로 변경
         setCurrentMonth(prevMonth);
-        const prevYear = currentYear-1;
+        const prevYear = currentYear - 1;
         setCurrentYear(prevYear);
       } else {
-        setCurrentMonth(currentMonth-1);
+        setCurrentMonth(currentMonth - 1);
       }
     }
 
@@ -53,22 +108,18 @@ export default function Calendar() {
       if(currentMonth === 11) { //12월이면
         const nextMonth = 0; //1월로 변경
         setCurrentMonth(nextMonth);
-        const nextYear = currentYear+1;
+        const nextYear = currentYear + 1;
         setCurrentYear(nextYear);
       } else {
-        setCurrentMonth(currentMonth+1);
+        setCurrentMonth(currentMonth + 1);
       }
     }
 
-    //남은 그리드를 채우기 위한 다음 달 날짜
-    const totalCells = 42;
-    const remainCells = totalCells - (prevMonthDates.length + currentMonthDates.length);    
-    const nextMonthDates = Array.from({length: remainCells}, (_,i)=> i + 1);
-    
+    //전체 날짜 데이터 
     const calendar = [
-      ...prevMonthDates.map(date => ({ date, currentMonth: false })),
-      ...currentMonthDates.map(date => ({ date, currentMonth: true })),
-      ...nextMonthDates.map(date => ({ date, currentMonth: false })),
+      ...prevMonthDatesArr,
+      ...currentMonthDatesArr,
+      ...nextMonthDatesArr,
     ];
     
     return (
@@ -101,16 +152,24 @@ export default function Calendar() {
 
             <ul className="auto-rows-[62px] grid grid-cols-7 gap-8 ">
               {/* days */}
-              {calendar.map((date,i) => (
-                <li
-                  key={i}
-                  className={`border ${date.currentMonth ? 'text-gray-700' : 'text-gray-400'} p-9 cursor-pointer border-gray-300 pt-8 rounded-lg body-14-400 flex flex-col items-center justify-start`}>
-                    {date.date}
-                    {/* 조건부 렌더링*/}
-                    <div
-                      className='w-52 h-8 mt-14 rounded-xl bg-blue-200' />
-                  </li>
-              ))}
+
+              {calendar.map((item) => {
+                  return (
+                    <li
+                    key={item.date}
+                    className={`border ${item.isCurrentMonth ? 'text-gray-700' : 'text-gray-400'} p-9 cursor-pointer border-gray-300 pt-8 rounded-lg body-14-400 flex flex-col items-center justify-start`}>
+                      {item.date[8] === '0' ? item.date.slice(9) : item.date.slice(-2)}
+                      {schedules &&
+                        workInfoArr.map((shift,i) => (
+                          shift.shiftDate === item.date 
+                          ? <div key={i} className='w-52 h-8 mt-14 rounded-xl bg-blue-200' />
+                          : ''
+                      ))}
+  
+                    </li>
+                  )
+              })}
+
             </ul>
           </div>
         </>
