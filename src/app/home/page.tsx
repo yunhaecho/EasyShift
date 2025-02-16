8ac10583af38faf3f948ac4ca275267b1d9ae3e9
soyeon <jayy_19@ewhain.net>
2025-02-16 19:15:22 +0900

    Feat: Add empty state page for stores

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,13 +2,18 @@
 
 import { useState } from 'react';
 import ShareIcon from '../../assets/icons/share.svg';
+import StoreIcon from '../../assets/icons/store.svg';
+import PlusIcon from '../../assets/icons/plus.svg';
 import WeeklyCalendar from './components/WeeklyCalendar';
 import WeeklyNavigator from './components/WeeklyNavigator';
 
 const HomePage = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  return (
+  /* 임시 */
+  const isStoreExist = false;
+
+  return isStoreExist ? (
     <div className="flex w-full flex-col gap-14 px-32 py-14">
       <div className="flex h-42 items-center justify-between">
         <div className="flex h-full gap-12">
@@ -29,6 +34,20 @@ const HomePage = () => {
         </button>
       </div>
       <WeeklyCalendar currentDate={currentDate} />
+    </div>
+  ) : (
+    <div className="flex h-full w-full flex-col items-center justify-center gap-22">
+      <StoreIcon />
+      <div className="flex flex-col items-center gap-4">
+        <p className="body-18-500 text-gray-900">Please create a store</p>
+        <p className="body-14-400 text-gray-600">
+          Get started by creating your first store
+        </p>
+      </div>
+      <button className="flex items-center gap-12 rounded-4 border border-gray-400 bg-gray-900 bg-white px-16 py-8">
+        <PlusIcon />
+        <p className="body-16-400 text-white">Create Store</p>
+      </button>
     </div>
   );
 };
