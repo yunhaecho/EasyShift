e679645024b1a2c34ca8bbd52f9dc31bf4c2031b
soyeon <jayy_19@ewhain.net>
2025-02-17 18:44:34 +0900

    Feat: Add store information UI

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   /* 임시 */
-  const isStoreExist = false;
+  const isStoreExist = true;
 
   return isStoreExist ? (
     <div className="flex w-full flex-col gap-14 px-32 py-14">
@@ -49,7 +49,7 @@ const HomePage = () => {
           </p>
         </div>
         <button
-          className="flex items-center gap-12 rounded-4 border border-gray-400 bg-gray-900 bg-white px-16 py-8"
+          className="flex items-center gap-12 rounded-4 bg-gray-900 px-16 py-8"
           onClick={() => setIsModalOpen(true)}
         >
           <PlusWhiteIcon />
