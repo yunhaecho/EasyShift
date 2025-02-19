740ab3a27829e213e0bca41f4e8d203a35cecac7
soyeon <jayy_19@ewhain.net>
2025-02-19 19:36:59 +0900

    Feat: Set up MSW for API mocking

--- src/mocks/handlers/storeHandlers.ts
+++ src/mocks/handlers/storeHandlers.ts
@@ -20,71 +20,49 @@ export const storeHandlers = [
         scheduleName: 'Barista',
         shifts: [
           {
+            id: 201,
+            shiftDate: '2025-2-19',
             shiftName: 'Morning',
             startTime: '08:00',
             endTime: '12:00',
-            dates: [
+            assignedUser: [
               {
-                id: 201,
-                shiftDate: '2025-2-19',
-                assignedUser: [
-                  {
-                    id: 1,
-                    name: '손태인',
-                  },
-                  {
-                    id: 2,
-                    name: '조장호',
-                  },
-                ],
+                id: 1,
+                name: '손태인',
               },
               {
-                id: 202,
-                shiftDate: '2025-2-20',
-                assignedUser: [
-                  {
-                    id: 3,
-                    name: '김찬호',
-                  },
-                ],
+                id: 2,
+                name: '조장호',
               },
             ],
           },
           {
-            shiftName: 'Middle',
+            id: 202,
+            shiftDate: '2025-2-19',
+            shiftName: 'Afternoon',
             startTime: '12:00',
             endTime: '16:00',
-            dates: [
+            assignedUser: [
               {
-                id: 203,
-                shiftDate: '2025-2-19',
-                assignedUser: [
-                  {
-                    id: 4,
-                    name: '고주형',
-                  },
-                  {
-                    id: 5,
-                    name: '조윤해',
-                  },
-                ],
+                id: 3,
+                name: '김찬호',
               },
             ],
           },
           {
+            id: 203,
+            shiftDate: '2025-2-19',
             shiftName: 'Night',
             startTime: '16:00',
             endTime: '20:00',
-            dates: [
+            assignedUser: [
               {
-                id: 204,
-                shiftDate: '2025-2-20',
-                assignedUser: [
-                  {
-                    id: 6,
-                    name: '양소연',
-                  },
-                ],
+                id: 4,
+                name: '고주형',
+              },
+              {
+                id: 5,
+                name: '조윤해',
               },
             ],
           },
