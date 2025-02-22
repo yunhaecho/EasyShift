569c4b07da2f943e97991c62acc9c7c54b93b159
yuunhae <susu492@naver.com>
2025-02-23 03:46:50 +0900

    feat: date-fns 적용하여 utils 함수 정리

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,5 +1,13 @@
 import { WeekDates } from '@/app/stores/[storeId]/home/types';
-import { startOfWeek, addDays, format } from 'date-fns';
+import {
+  startOfWeek,
+  addDays,
+  format,
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+  endOfWeek,
+} from 'date-fns';
 
 /**
  * 오늘 기준 이번 주 날짜 정보 반환
@@ -20,74 +28,30 @@ export const getCurrentWeekDates = (currentDate = new Date()) => {
   }) as WeekDates;
 };
 
-export const convertMonthToNumber = (month: string) => {
-  return month
-    .replace('Jan', '01')
-    .replace('Feb', '02')
-    .replace('Mar', '03')
-    .replace('Apr', '04')
-    .replace('May', '05')
-    .replace('Jun', '06')
-    .replace('Jul', '07')
-    .replace('Aug', '08')
-    .replace('Sep', '09')
-    .replace('Oct', '10')
-    .replace('Nov', '11')
-    .replace('Dec', '12');
-};
-
 /**
  * 현재 월 기준으로 현재 월 날짜 전제, 이전 달 날짜 일부, 다음 달 날짜 일부 반환
  * @returns 현재 월 기준  [이전 달 마지막 주 날짜, 현재 월 날짜 전체, 다음 날 첫 주 날짜] 배열
  */
 
-export const generateCalendar  = (currentYear: number, currentMonth: number ) => {
+export const generateCalendar = (start: Date) => {
+  //현재 월
+  const fstOfMonth = startOfMonth(start);
+  const lastOfMonth = endOfMonth(start);
 
-  //현재 월 첫 날의 요일 : 0(일요일) ~ 6(토요일)
-  const fstOfCurrentMonth = new Date(currentYear, currentMonth , 1).getDay(); 
-  
-  //현재 월 마지막 날짜
-  const currentMonthLastDate = new Date(currentYear, currentMonth , 0).getDate();
+  const startOfCalendar = startOfWeek(fstOfMonth);
+  const endOfCalendar = endOfWeek(lastOfMonth);
 
-  //현재 월 날짜 배열
-  const currentMonthDates = Array.from({ length: currentMonthLastDate }, (_,index) => index + 1); 
-  const currentMonthDatesArr = currentMonthDates.map( day => ({
-    date : `${format(new Date(currentYear, currentMonth, day), 'yyyy-MM-dd')}`, 
-    isCurrentMonth: true
+  const datesOfCurrentMonth = eachDayOfInterval({
+    start: startOfCalendar,
+    end: endOfCalendar,
+  });
+
+  const calendarDates = datesOfCurrentMonth.map(unformattedDate => ({
+    //Wed Jan 29 2025 00:00:00 GMT+0900 (한국 표준시)
+    unformattedDate,
+    formattedDate: format(unformattedDate, 'yyyy-MM-dd'),
+    isCurrentMonth: unformattedDate.getMonth() === fstOfMonth.getMonth(),
   }));
 
-  //이전 연도, 월 (currentMonth가 0이면 currentYear - 1 처리)
-  const prevYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-  const prevMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-
-  // 이전 월 일부 날짜 배열
-  const prevMonthDates = Array.from({length: fstOfCurrentMonth}, (_,i)=> currentMonthDates.length - fstOfCurrentMonth + i + 1);
-  const prevMonthDatesArr = prevMonthDates.map( day => ({
-      date: `${format(new Date(prevYear,prevMonth,day), 'yyyy-MM-dd')}`,
-      isCurrentMonth: false
-  }));
-
-  //다음 연도, 월 (currentMonth가 11이면 currentYear + 1 처리)
-  const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
-  const nextYear = currentMonth === 11 ? currentYear + 1 : currentYear;
-
-  //남은 그리드를 채우기 위한 다음 달 날짜
-  const totalCells = 42;
-  const remainCells = totalCells - (prevMonthDates.length + currentMonthDates.length);    
-  const nextMonthDates = Array.from({length: remainCells}, (_,i)=> i + 1);
-
-  // 다음 월 일부 날짜 배열
-  const nextMonthDatesArr = nextMonthDates.map(day => ({
-    date : `${format(new Date(nextYear, nextMonth, day), 'yyyy-MM-dd')}`, 
-    isCurrentMonth: false 
-  }))
-
-  //전체 날짜 데이터 
-  const calendarDates = [
-    ...prevMonthDatesArr,
-    ...currentMonthDatesArr,
-    ...nextMonthDatesArr,
-  ];
-  
   return calendarDates;
-}
+};
