73b98a98c55d0cd34ac65c38d6a801c6c1d8e575
yuunhae <susu492@naver.com>
2025-02-23 04:04:18 +0900

    Chore  : 불필요한 주석 제거

--- src/app/workers/components/Calendar.tsx
+++ src/app/workers/components/Calendar.tsx
@@ -14,11 +14,6 @@ export default function Calendar() {
   //캘린더에 표시되는 모든 날짜
   const daysInCalendar = generateCalendar(new Date(currentYear, currentMonth));
 
-  // const weekName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']; // 리렌더 될 때 마다,불필요함. contants로 빼기 ✅
-
-  //useMemo 사용하기 : 캐싱 계산하는 개 무거울 때 스면 좋음 ✅
-  // const monthName = Array.from({length:12},(_, index) => format(new Date(currentYear, index), 'MMMM')); //"Febuary"
-
   //mock data
   const [schedules] = useState([
     {
@@ -33,10 +28,6 @@ export default function Calendar() {
     },
   ]);
 
-  //workDate가 없을 떄는 안 돌게 하는거 : useSuspenceQuery(), 데이터 안에 undefinded가 안뜸. 렌더링 중단시킴
-  //그 대신 fallback을 그림. 데이터가 오기 전 까지는 Contents를 안 그림 데이터가 무조건 있다는 전제하에 코드를 짜니까 코드짜기가 편해짐.
-
-  //useMemo 사용하기 : 캐싱 계산하는 개 무거울 때 스면 좋음 ✅
   const workerSchedule = useMemo(
     () =>
       schedules.flatMap(schedule =>
@@ -53,7 +44,6 @@ export default function Calendar() {
   const clickNext = () => goToPrevOrNextMonth(+1);
 
   return (
-    //fragment사용할 떄 하나로 묶어주면 좋긴함. ✅
     <div>
       {/* CalendarHeader  */}
       <div className="mb-26 flex h-32 flex-row justify-between">
@@ -93,13 +83,11 @@ export default function Calendar() {
             return (
               <li
                 key={eachDay.unformattedDate.getTime()}
-                // classNames로 분리 ✅
                 className={classNames(
                   'body-14-400 flex cursor-pointer flex-col items-center justify-start rounded-lg border border-gray-300 p-9 pt-8',
                   `${eachDay.isCurrentMonth ? 'text-gray-700' : 'text-gray-400'}`,
                 )}
               >
-                {/*1. 코드 의도 알기가 어려움 2.백엔드에서 주는 데이터가 바뀌면 깨지는 코드가 됨 3.이터레이터명 바꾸기 4. new Date(date.date).getDate() ✅*/}
                 {`${eachDay.unformattedDate.getDate()}`}
                 {workerSchedule.map(datesOfWorkDay => {
                   if (datesOfWorkDay === eachDay.formattedDate) {
