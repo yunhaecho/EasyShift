2103571355556008d2282ecbd8448d7304db5d30
soyeon <jayy_19@ewhain.net>
2025-02-24 15:15:38 +0900

    Refactor: Remove unused states and function

--- src/app/stores/[storeId]/home/hooks/useWeeklyCalendar.ts
+++ src/app/stores/[storeId]/home/hooks/useWeeklyCalendar.ts
@@ -7,7 +7,6 @@ import { addWeeks } from 'date-fns';
 
 const useWeeklyCalendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [isWorkerInfoModalOpen, setIsWorkerInfoModalOpen] = useState(false);
 
   const currentWeekDates = useMemo(
     () => getCurrentWeekDates(currentDate),
@@ -22,18 +21,12 @@ const useWeeklyCalendar = () => {
     setCurrentDate(prevDate => addWeeks(prevDate, 1));
   }, []);
 
-  const toggleWorkerInfoModal = useCallback(() => {
-    setIsWorkerInfoModalOpen(prev => !prev);
-  }, []);
-
   return {
     currentDate,
     setCurrentDate,
     currentWeekDates,
     goToPreviousWeek,
     goToNextWeek,
-    isWorkerInfoModalOpen,
-    toggleWorkerInfoModal,
   };
 };
 
