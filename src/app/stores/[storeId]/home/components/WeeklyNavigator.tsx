e3df55893118ef41bdc5273ccdc17fe69c36d761
soyeon <jayy_19@ewhain.net>
2025-02-27 14:20:11 +0900

    Fix: Resolve duplicated issue

--- src/app/stores/[storeId]/home/components/WeeklyNavigator.tsx
+++ src/app/stores/[storeId]/home/components/WeeklyNavigator.tsx
@@ -4,7 +4,6 @@ import SelectedStoreDropdown from './SelectedStoreDropdown';
 
 import ChevronLeftIcon from '@/assets/icons/chevron-left.svg';
 import ChevronRightIcon from '@/assets/icons/chevron-right.svg';
-import { WeekDates } from '../types';
 
 const WeeklyNavigator = ({
   homeData,
