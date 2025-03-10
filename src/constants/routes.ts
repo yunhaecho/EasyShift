273b03afd0d994b398c0163e9dc0ab4497f50006
yuunhae <susu492@naver.com>
2025-03-11 03:49:50 +0900

    Feat : query params로 카카오 로그인 연결

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
   LANDING: 'landing',
-  SIGNIN: 'signin',
+  SIGNIN: `${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`,
   SIGNUP: 'signup',
   STORES: 'stores',
   HOME: 'home',
