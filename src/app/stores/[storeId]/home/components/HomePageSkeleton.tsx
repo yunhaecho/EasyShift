536bcf053885e3c3d117aeb0914119b475e39884
soyeon <jayy_19@ewhain.net>
2025-03-11 10:53:21 +0900

    Feat: Add schedule detail page skeleton ui

--- src/app/stores/[storeId]/home/components/HomePageSkeleton.tsx
+++ src/app/stores/[storeId]/home/components/HomePageSkeleton.tsx
@@ -1,21 +1,18 @@
-import ChevronLeftIcon from '@/assets/icons/chevron-left.svg';
-import ChevronRightIcon from '@/assets/icons/chevron-right.svg';
+import ChevronLeftLightGrayIcon from '@/assets/icons/chevron-left-lightgray.svg';
+import ChevronRightLightGrayIcon from '@/assets/icons/chevron-right-lightgray.svg';
 
 const HomePageSkeleton = () => {
   return (
     <div className="flex w-full flex-col gap-14">
       <nav className="flex h-42 w-full items-center justify-between">
         <div className="flex items-center gap-16">
-          <ChevronLeftIcon className="mb-5 h-40 w-26" />
+          <ChevronLeftLightGrayIcon className="mb-5 animate-pulse" />
           <div className="h-40 w-170 animate-pulse rounded-4 bg-gray-200" />
-          <ChevronRightIcon className="mb-5 h-40 w-26" />
-          <div className="ml-16 h-40 w-70 animate-pulse rounded-4 border border-gray-400 bg-white" />
+          <ChevronRightLightGrayIcon className="mb-5 animate-pulse" />
         </div>
-        {/* toggle and dropdown */}
       </nav>
 
       <section className="w-full rounded-4 border border-gray-300 bg-white shadow-sm">
-        <h2 className="sr-only">오늘 날짜 기준 주간 근무 일정</h2>
         <table className="w-full table-fixed border-collapse">
           <thead>
             <tr>
@@ -28,31 +25,43 @@ const HomePageSkeleton = () => {
             </tr>
           </thead>
           <tbody>
-            {Array.from({ length: 3 }).map((_, rowIndex) => (
-              <tr key={rowIndex} className="border-t border-gray-400 align-top">
-                <td className="p-16">
-                  <div className="h-20 w-40 animate-pulse rounded-4 bg-gray-200" />
-                  <div className="mt-4 h-16 w-80 animate-pulse rounded-4 bg-gray-200" />
-                </td>
-                {Array.from({ length: 7 }).map((_, colIndex) => (
-                  <td
-                    key={`${rowIndex}-${colIndex}`}
-                    className="h-162 border-l border-gray-400 p-16 align-top"
-                  >
-                    <div className="flex flex-col gap-8">
-                      {Array.from({
-                        length: Math.floor(Math.random() * 2) + 1,
-                      }).map((_, blockIndex) => (
-                        <div
-                          key={blockIndex}
-                          className="h-40 w-full animate-pulse rounded-4 bg-gray-200"
-                        />
-                      ))}
-                    </div>
+            {Array.from({ length: 3 }).map((_, rowIndex) => {
+              const bgColor =
+                rowIndex % 3 === 0
+                  ? 'bg-primary-100'
+                  : rowIndex % 3 === 1
+                    ? 'bg-orange-100'
+                    : 'bg-green-100';
+
+              return (
+                <tr
+                  key={rowIndex}
+                  className="border-t border-gray-400 align-top"
+                >
+                  <td className="p-16">
+                    <div className="h-20 w-40 animate-pulse rounded-4 bg-gray-200" />
+                    <div className="mt-4 h-16 w-80 animate-pulse rounded-4 bg-gray-200" />
                   </td>
-                ))}
-              </tr>
-            ))}
+                  {Array.from({ length: 7 }).map((_, colIndex) => (
+                    <td
+                      key={`${rowIndex}-${colIndex}`}
+                      className={`h-162 border-l border-gray-400 p-16 align-top ${bgColor}`}
+                    >
+                      <div className="flex flex-col gap-8">
+                        {Array.from({
+                          length: Math.floor(Math.random() * 2) + 1,
+                        }).map((_, blockIndex) => (
+                          <div
+                            key={blockIndex}
+                            className="h-40 w-full animate-pulse rounded-4 bg-gray-100 opacity-50"
+                          />
+                        ))}
+                      </div>
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </section>
--- src/app/stores/[storeId]/schedule/[scheduleId]/page.tsx
+++ src/app/stores/[storeId]/schedule/[scheduleId]/page.tsx
@@ -6,24 +6,28 @@ import WeeklyNavigator from './components/WeeklyNavigator';
 import WeeklyCalendar from './components/WeeklyCalendar';
 import WorkersListSidebar from './components/WorkersListSidebar';
 import ScheduleDetailPageProvider from './components/ScheduleDetailPageProvider';
+import { Suspense } from 'react';
+import ScheduleDetailPageSkeleton from './components/ScheduleDetailPageSkeleton';
 
 const ScheduleDetailPage = () => {
   const [isUserInfoModalOpen, toggleUserInfoModal] = useToggle();
 
   return (
-    <ScheduleDetailPageProvider>
-      <main className="flex w-full flex-col gap-14 px-32 py-14">
-        <WeeklyNavigator />
-        <WeeklyCalendar />
-      </main>
-      <WorkersListSidebar />
+    <Suspense fallback={<ScheduleDetailPageSkeleton />}>
+      <ScheduleDetailPageProvider>
+        <main className="flex w-full flex-col gap-14 px-32 py-14">
+          <WeeklyNavigator />
+          <WeeklyCalendar />
+        </main>
+        <WorkersListSidebar />
 
-      <UserInfoModal
-        isOpen={isUserInfoModalOpen}
-        onClose={toggleUserInfoModal}
-        userId={401} // [TODO]: Edit this
-      />
-    </ScheduleDetailPageProvider>
+        <UserInfoModal
+          isOpen={isUserInfoModalOpen}
+          onClose={toggleUserInfoModal}
+          userId={401} // [TODO]: Edit this
+        />
+      </ScheduleDetailPageProvider>
+    </Suspense>
   );
 };
 
