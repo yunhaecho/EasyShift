440d4e93fee774cb105cad5c47658a7f86bdbd4e
yuunhae <susu492@naver.com>
2025-10-15 20:07:53 +0900

    refactor: AuthContext를 NextAuth 세션 상태와 일치하도록 단순화

--- src/app/context/AuthContext.ts
+++ src/app/context/AuthContext.ts
@@ -1,15 +1,12 @@
-import { User, UserRole } from '@/api/endpoints/user/types';
-import { USER_ROLE } from '@/constants/userRole';
+import { User } from '@/api/endpoints/user/types';
 import { createContext } from 'react';
 
 export const AuthContext = createContext<{
   user: User | null;
   setUser: (user: User | null) => void;
-  isAuthenticated: boolean;
-  userRole: UserRole;
+  status: 'authenticated' | 'loading' | 'unauthenticated';
 }>({
   user: null,
   setUser: () => {},
-  isAuthenticated: false,
-  userRole: USER_ROLE.GUEST,
+  status: 'unauthenticated',
 });
