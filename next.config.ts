909f76b7613455ad29ffaa17cf535229f9c717cf
yuunhae <susu492@naver.com>
2025-10-22 20:52:50 +0900

    feat : @next/bundle analyzer 추가

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
 import type { NextConfig } from 'next';
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: true,
+});
 
 const nextConfig: NextConfig = {
   webpack(config, context) {
@@ -29,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
